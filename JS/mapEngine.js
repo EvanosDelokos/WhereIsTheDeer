e791885dd4bef92a83a2ebc256bc9e1596e945ab
Evanos Delokos <evankoka12@gmail.com>
2025-07-11 21:55:29 +1000

    Add B2 overlays to mapEngine

--- JS/mapEngine.js
+++ JS/mapEngine.js
@@ -3,6 +3,20 @@ console.log("Module loaded: mapEngine");
 document.addEventListener("DOMContentLoaded", () => {
   const map = L.map('map').setView([-36.5, 146.5], 7);
 
+  // After map init
+fetch('https://f004.backblazeb2.com/file/whereisthedeer/LocalityPolygon.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data).addTo(map);
+  });
+
+fetch('https://f004.backblazeb2.com/file/whereisthedeer/zones.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data).addTo(map);
+  });
+
+
   // Optional: Add scale control
   L.control.scale({ position: 'bottomright' }).addTo(map);
 
